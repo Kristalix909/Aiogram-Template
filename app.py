8eff282e83683024087fed2d5e42d4c2860c9fe3
Kristalix909 <98950080+Kristalix909@users.noreply.github.com>
2022-08-29 21:46:20 +0500

    Update app.py

--- app.py
+++ app.py
@@ -7,16 +7,16 @@ from utils.set_bot_commands import set_default_commands
 
 
 async def on_startup(dispatcher):
-    # Birlamchi komandalar (/start va /help)
+  
     await set_default_commands(dispatcher)
 
-    # Ma'lumotlar bazasini yaratamiz:
+  
     try:
         db.create_table_users()
     except Exception as err:
         print(err)
 
-    # Bot ishga tushgani haqida adminga xabar berish
+   
     await on_startup_notify(dispatcher)
 
 
