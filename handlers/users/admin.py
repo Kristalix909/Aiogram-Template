03dca4f817260e728a3c9f5bd2f6365c01de2d87
Kristalix909 <98950080+Kristalix909@users.noreply.github.com>
2022-08-29 21:44:21 +0500

    Update admin.py

--- handlers/users/admin.py
+++ handlers/users/admin.py
@@ -30,10 +30,10 @@ async def send_ad_to_all(message: types.Message):
     users = db.select_all_users()
     for user in users:
         user_id = user[0]
-        await bot.send_message(chat_id=user_id, text="@BekoDev kanaliga obuna bo'ling!")
+        await bot.send_message(chat_id=user_id, text="Реклама!")
         await asyncio.sleep(0.05)
 
 @dp.message_handler(text="/cleandb", user_id=ADMINS)
 async def get_all_users(message: types.Message):
     db.delete_users()
-    await message.answer("Baza tozalandi!")
+    await message.answer("База пусто!")
